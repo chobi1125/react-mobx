83edfd626b3ad344d8454170076b28a1f8ed2a7f
chobi1125 <chonmage112000@yahoo.co.jp>
2020-12-12 16:07:14 +0900

    injectを使ったサンプル

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,20 @@
 import "../css/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'mobx-react';
 
 import TodoList from "./TodoList";
+import TodoListInject from "./TodoListInject";
 import store from "./TodoStore";
 
 const app = document.getElementById("app");
 
-ReactDOM.render(<TodoList store={store} />, app);
+ReactDOM.render(
+    // <TodoList store={store} />
+    <>
+        <Provider store={store}>
+            {/* <TodoList store={store} /> */}
+            <TodoListInject /> 
+        </Provider>
+    </>
+,app);
